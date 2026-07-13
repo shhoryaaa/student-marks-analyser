e3bc3dc80d0f04aae8948062b6e4c72cd6d20cb5
shhoryaaa <shhoryagarwal@gmail.com>
2026-07-14 01:51:50 +0530

    mean

--- index.py
+++ index.py
@@ -28,10 +28,38 @@ def Userout(Id):
     print("Student not found.")
 
 
+def Mean(Subj):
+    total = 0
+    count = 0
+
+    for student in lst:
+        if Subj == 1:
+            total += student["Marks1"]
+        elif Subj == 2:
+            total += student["Marks2"]
+        elif Subj == 3:
+            total += student["Marks3"]
+        else:
+            print("Invalid Subject")
+            return
+        count += 1
+    if count == 0:
+        print("No student records found.")
+        return
+    mean = total / count
+    print("Mean =", mean)
+
+def Median(Subj):
+    n=len(lst)
+    marks.sort()
+    
+
+
 lst = []
 
 while True:
-    print("\n1 = Enter Student Mark Details")
+    print("\n Student Marks Analyser")
+    print("1 = Enter Student Mark Details")
     print("2 = See Student Mark Details")
     print("3 = Analyse Marks")
     print("4 = End")
@@ -62,7 +90,41 @@ while True:
         Userout(b)
 
     elif a == 3:
-        print("Analysis function coming soon.")
+        print("\n========== Analysis Menu ==========")
+        print("1 = Mean")
+        print("2 = Median")
+        print("3 = Mode")
+        print("4 = Highest Marks")
+        print("5 = Lowest Marks")
+        print("6 = Standard Deviation")
+        print("7 = Variance")
+
+        b = int(input("Choose an option: "))
+
+        if b == 1:
+            c = int(input("Please select Subject (1/2/3): "))
+            Mean(c)
+
+        elif b == 2:
+            print("Median function coming soon.")
+
+        elif b == 3:
+            print("Mode function coming soon.")
+
+        elif b == 4:
+            print("Highest Marks function coming soon.")
+
+        elif b == 5:
+            print("Lowest Marks function coming soon.")
+
+        elif b == 6:
+            print("Standard Deviation function coming soon.")
+
+        elif b == 7:
+            print("Variance function coming soon.")
+
+        else:
+            print("Invalid Choice!")
 
     elif a == 4:
         print("Program Ended.")
